015e1cbecab59f517cad7a31f9346ba1d73028ac
Luís Filipe Guedes Mota <luisfilipe.mota@geoambiente.com.br>
2019-06-05 10:49:30 -0300

    Conversão do Projeto para mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotNetCore_Web_API_Jenkins_Azure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
